9573ef89d1403bb0f90d41d9b606433c2068513d
m-orlewski <morlewski@protonmail.com>
2021-11-13 16:12:07 +0100

    add home page templates

--- crypto_portfolio/portfolio/urls.py
+++ crypto_portfolio/portfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='portfolio-home'),
+    path('about/', views.about, name='portfolio-about'),
 ]
--- crypto_portfolio/portfolio/views.py
+++ crypto_portfolio/portfolio/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'portfolio/home.html')
+    return render(request, 'portfolio/home.html', {'title': 'Home page'})
+    
+def about(request):
+    return render(request, 'portfolio/about.html', {'title': 'About page'})
