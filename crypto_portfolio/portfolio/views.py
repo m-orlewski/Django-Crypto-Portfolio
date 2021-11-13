3fe40f7df5263f566265f4ae3f07e1f82c4af149
m-orlewski <morlewski@protonmail.com>
2021-11-13 13:56:54 +0100

    home page view

--- crypto_portfolio/portfolio/views.py
+++ crypto_portfolio/portfolio/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Portfolio home page</h1>')
+    return render(request, 'portfolio/home.html')
