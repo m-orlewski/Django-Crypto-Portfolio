63a9b2df5fdee4edb114f7e8f942744b946ba8ce
KamilKapron <kapron.kam@gmail.com>
2021-11-21 14:02:32 +0100

    Delete accout model

--- crypto_portfolio/portfolio/models.py
+++ crypto_portfolio/portfolio/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 from django.utils import timezone
-
-class Account(models.Model):
-    login = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(default = 'from@example.com',unique=True)
-    password = models.CharField(max_length=100)
-    value = models.FloatField()
-    date_of_joining = models.DateTimeField(default=timezone.now)
-
-
-    def __str__(self):
-        return self.login + " - wartosc portfela: " + self.value
