f654eadaefc5b548829d221cf29a88e2ea7cbe00
m-orlewski <morlewski@protonmail.com>
2021-12-27 18:13:21 +0100

    add website plots

--- crypto_portfolio/portfolio/views.py
+++ crypto_portfolio/portfolio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from . import portfolio_utils as pu
+from .utils import *
 
 from .models import Asset, Portfolio
 
@@ -9,9 +10,18 @@ def home(request):
 
 def profile(request):
     current_user = request.user
-    current_portfolio = Portfolio.objects.get(user=current_user)
-    user_assets = Asset.objects.filter(portfolio=current_portfolio)
-    return render(request, 'portfolio/portfolio.html', context={ 'assets': user_assets})
+    
+    try:
+        current_portfolio = Portfolio.objects.get(user=current_user)
+        user_assets = Asset.objects.filter(portfolio=current_portfolio)
+    except:
+        user_assets = {}
+
+    data = pu.make_request()
+
+    chart = get_plot([1,2,3], [1,4,9])
+
+    return render(request, 'portfolio/portfolio.html', context={ 'assets': user_assets, 'api_data': data, 'chart': chart})
 
 def about(request):
     return render(request, 'portfolio/about.html', {'title': 'About page'})
